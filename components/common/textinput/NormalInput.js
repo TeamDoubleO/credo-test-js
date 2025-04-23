162e1063ebfd8ce07643ff87853109aa0ab6cf07
zzzzzisu <84431156+js4939@users.noreply.github.com>
2025-04-23 20:24:48 +0900

    KW-145/feat: 커스텀 TextInput 컴포넌트 구현 (#5)

---
+++ components/common/textinput/NormalInput.js
@@ -0,0 +1,30 @@
+import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { styles } from './styles/NormalInput.styles';
+import { TextInput } from 'react-native';
+
+const NormalInput = ({
+  placeholder = 'placeholder',
+  errorText = '',
+  value,
+  onChangeTextHandler,
+}) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  return (
+    <View>
+      {errorText !== '' && <Text style={styles.errorText}>{errorText}</Text>}
+      <TextInput
+        placeholder={placeholder}
+        placeholderTextColor="#7E7E7E"
+        value={value}
+        onChangeText={onChangeTextHandler}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        style={[styles.input, isFocused && styles.focused, errorText && styles.error]}
+      ></TextInput>
+    </View>
+  );
+};
+
+export default NormalInput;
---
+++ components/common/textinput/styles/NormalInput.styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  input: {
+    width: 316,
+    height: 50,
+    marginBottom: 20,
+    paddingHorizontal: 15,
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: '#BCCBBE',
+    fontSize: 17,
+  },
+  focused: { borderColor: '#385E3C' },
+  error: { borderColor: '#FF4943' },
+  errorText: { margin: 10, marginTop: 0, fontSize: 15, color: '#FF4943' },
+});
