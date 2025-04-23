98776e77d8a62a0e31f7beceabfa9dbabbff4170
zzzzzisu <84431156+js4939@users.noreply.github.com>
2025-04-23 09:13:12 +0900

    [KW-72] chore/project initial setting (#2)
    * 프로젝트 초기 설정
    
    * commit test
    
    * KW-72/chore: 프로젝트 초기 설정 완료

---
+++ App.js
@@ -0,0 +1,20 @@
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View style={styles.sample}>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  sample: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
