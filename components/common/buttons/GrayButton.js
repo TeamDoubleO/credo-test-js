129e63481aa525d9c608a74cba10ba9a872a873f
Jiwon Chae <63784453+jiwon0226@users.noreply.github.com>
2025-04-25 15:24:34 +0900

    KW-166/feat: 회색 밑줄 버튼 컴포넌트 구현 (#14)

---
+++ components/common/buttons/GrayButton.js
@@ -0,0 +1,20 @@
+import { Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { styles } from './styles/GrayButton.styles';
+
+const GrayButton = ({
+  title, // 버튼 텍스트
+  onPressHandler, // 버튼 클릭 시 핸들러 함수
+  style, // 외부 스타일
+}) => {
+  return (
+    <TouchableOpacity 
+      onPress={onPressHandler}
+      style={[styles.button, style]} 
+    >
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default GrayButton;
