8371f8da0f4e5415dd0348bd4e1529d837fa6e85
zzzzzisu <84431156+js4939@users.noreply.github.com>
2025-04-25 13:09:32 +0900

    KW-153/feat/welcome page (#13)
    * KW-153/feat: WelcomePage 구현 및 Navigator 연결
    
    * KW-153/fix: WelcomePage 컴포넌트 간격 %로 수정

--- pages/styles/WelcomePage.styles.js
+++ pages/styles/WelcomePage.styles.js
@@ -2,18 +2,17 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    marginTop: 150, // 헤더 사라진 길이 만큼 margin 지정
-    marginLeft: 5,
-    paddingVertical: 20,
+    marginTop: '30%', // 헤더 사라진 길이 만큼 margin 지정
+    paddingVertical: '5%',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  logoTextImage: { width: 220, height: 60, marginLeft: 5, marginBottom: 20 },
-  text: { fontSize: 20, color: '#7E7E7E', marginRight: 2 },
-  logoIconImage: { width: 230, height: 200, marginLeft: 5, marginVertical: 80 },
+  logoTextImage: { width: '60%', height: '10%', marginBottom: '5%' },
+  text: { fontSize: 20, color: '#7E7E7E' },
+  logoIconImage: { width: '60%', height: '30%', marginVertical: '25%' },
   buttonContainer: {
     justifyContent: 'center',
     flexDirection: 'row',
-    gap: 15,
+    gap: '3%',
   },
 });
