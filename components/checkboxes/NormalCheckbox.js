d9ff4f6200dd477ee8937c71f0fd3f723b39f67f
zzzzzisu <84431156+js4939@users.noreply.github.com>
2025-05-08 17:14:37 +0900

    KW-319/fix: 체크박스 하나만 선택되도록 수정 (#46)

--- components/checkboxes/NormalCheckbox.js
+++ components/checkboxes/NormalCheckbox.js
@@ -4,24 +4,27 @@ import { styles } from './styles/NormalCheckbox.styles';
 import { Ionicons } from '@expo/vector-icons';
 
 const NormalCheckbox = ({ labels, onChangeHandler, style }) => {
-  const [checkedList, setCheckedList] = useState(Array(labels.length).fill(false));
+  const [selectedIndex, setSelectedIndex] = useState(null);
 
   // 체크박스 토글 관리 함수
   const handleToggle = (index) => {
-    const updatedList = [...checkedList];
-    updatedList[index] = !updatedList[index];
-    setCheckedList(updatedList);
+    const newIndex = selectedIndex === index ? null : index;
+    setSelectedIndex(newIndex);
 
-    // 부모 컴포넌트에 체크박스 상태 전달
-    if (onChangeHandler) onChangeHandler(updatedList);
+    // 부모 컴포넌트에 선택 상태 전달
+    if (onChangeHandler) {
+      // 선택된 항목만 true, 나머지는 false로 구성된 배열로 전달
+      const result = labels.map((_, i) => i === newIndex);
+      onChangeHandler(result);
+    }
   };
 
   return (
     <ScrollView contentContainerStyle={styles.contentContainer}>
       {labels.map((label, index) => (
         <TouchableOpacity key={index} style={styles.container} onPress={() => handleToggle(index)}>
-          <View style={[styles.checkbox, checkedList[index] && styles.checked, style]}>
-            {checkedList[index] && <Ionicons name="checkmark" color="white" />}
+          <View style={[styles.checkbox, selectedIndex === index && styles.checked, style]}>
+            {selectedIndex === index && <Ionicons name="checkmark" color="white" />}
           </View>
           <Text style={styles.label}>{label}</Text>
         </TouchableOpacity>
